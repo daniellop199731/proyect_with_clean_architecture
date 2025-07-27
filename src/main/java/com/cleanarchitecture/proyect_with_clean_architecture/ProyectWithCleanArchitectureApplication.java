1a968e7d60f0940d0e6d0f119bc9b1521d94a48a
Daniel Lopez <daniellop199731@gmail.com>
2025-07-27 12:12:28 -0500

    Init proyect

---
+++ src/main/java/com/cleanarchitecture/proyect_with_clean_architecture/ProyectWithCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.cleanarchitecture.proyect_with_clean_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectWithCleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectWithCleanArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cleanarchitecture/proyect_with_clean_architecture/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cleanarchitecture.proyect_with_clean_architecture;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProyectWithCleanArchitectureApplication.class);
+	}
+
+}
